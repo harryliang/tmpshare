7b44a1e5ea660650bb1012aea72beb731d7f9921
harry <harry.liang@idiaoyan.com>
2014-09-02 10:59:57 +0800

    add IP_CHECK switch

--- share/share_decorator.py
+++ share/share_decorator.py
@@ -8,10 +8,14 @@ import settings
 def ip_check(method):
     @functools.wraps(method)
     def wrapper(self, *args, **kwargs):
-        remote_ip = self.request.remote_ip
-        allowed_ip_list = set(json.load(file(os.path.join(settings.SITE_ROOT, 'config', 'allowed_ip.json'))))
-        if allowed_ip_list and remote_ip not in allowed_ip_list:
-            self.write("Access denied!")
-            return
+        if settings.IP_CHECK:
+            remote_ip = self.request.remote_ip
+            try:
+                allowed_ip_list = set(json.load(file(os.path.join(settings.SITE_ROOT, 'config', 'allowed_ip.json'))))
+            except IOError:
+                allowed_ip_list = None
+            if allowed_ip_list and remote_ip not in allowed_ip_list:
+                self.write("Access denied!")
+                return
         return method(self, *args, **kwargs)
     return wrapper
